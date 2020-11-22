1e5b6719e527035214b75e06f4588fe241c2f8a3
Nik <nn494@yandex.ru>
2020-11-22 16:13:30 +0300

    Yellow  Random Circles

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import sys
 from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5 import uic
 from random import randrange
+from UI import Ui_Form
 
 
-class Circles(QWidget):
+class Circles(QWidget, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.do_paint = False
         self.draw_area.paintEvent = self.paint_area
         self.btn.clicked.connect(self.paint)
 
     def paint_area(self, event):
         if self.do_paint:
-            print('some')
             qp = QPainter()
             qp.begin(self.draw_area)
-            qp.setPen(QColor(255, 255, 0))
+            qp.setPen(QColor(randrange(256), randrange(256), randrange(256)))
             radius = randrange(25, 100)
             x, y = randrange(radius + 1, 625 - radius), randrange(radius + 1, 425 - radius)
             qp.drawEllipse(x, y, radius, radius)
